949dcfcf4307277b634e70958974814f59a7a5bd
GGLVXD <gglvmax@gmail.com>
2025-03-05 18:58:23 +0200

    pareizi/nepareizi

---
+++ scenes/1/1.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_metals_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/1/pareizi.tscn")
+
+
+func _on_nemetals_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/1/nepareizi.tscn")
+func _on_oksids_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/1/nepareizi.tscn")
+func _on_skabe_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/1/nepareizi.tscn")
+func _on_sals_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/1/nepareizi.tscn")
+func _on_organs_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/1/nepareizi.tscn")
